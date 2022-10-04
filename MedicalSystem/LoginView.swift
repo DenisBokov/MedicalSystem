2d220b9f672f5916d13eb99e58456bd550f3f4ad
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-10-05 00:45:59 +0300

    work on the login screen

--- MedicalSystem/LoginView.swift
+++ MedicalSystem/LoginView.swift
@@ -14,27 +14,64 @@ struct LoginView: View {
     var body: some View {
         VStack {
             Text("Вход в систему")
-                .padding()
-                .font(.title.bold())
-                .background(Color(.systemGreen))
-                .cornerRadius(25)
+                .fontWeight(.heavy)
+                .font(.largeTitle)
+                .padding([.top, .bottom], 20)
+            
+            VStack(alignment: .leading) {
+                Text("Email")
+                    .font(.headline)
+                    .fontWeight(.light)
+                    .foregroundColor(Color.init(.label)).opacity(0.75)
+                HStack {
+                    TextField("Введите свой email", text: $auth.email)
+                    if auth.email != "" {
+                        Image("check")
+                            .foregroundColor(.gray)
+                    }
+                }
+                
+                Divider()
+                
+            }.padding(.bottom, 16)
+            
+            VStack(alignment: .leading) {
+                Text("Password")
+                    .font(.headline)
+                    .fontWeight(.light)
+                    .foregroundColor(Color.init(.label)).opacity(0.75)
+                
+                SecureField("Введите свой пароль", text: $auth.password)
+                
+                
+                Divider()
+                
+            }.padding(.bottom, 16)
             
             VStack {
-                TextField("Введите email", text: $auth.email)
-                    .padding()
-                    .font(.title.bold())
-                    .background(Color(.systemGreen))
-                    .cornerRadius(10)
-                    .padding()
+                Button(action: {}) {
+                    Text("Войти")
+                        .foregroundColor(.white)
+                        .frame(width: UIScreen.main.bounds.width - 120)
+                        .padding()
+                }.background(.green)
+                    .clipShape(Capsule())
+                    .padding(.top, 45)
                 
-                SecureField("Введите пароль", text: $auth.password)
-                    .padding()
-                    .font(.title.bold())
-                    .background(Color(.systemGreen))
-                    .cornerRadius(10)
-                    .padding()
+                Text("или")
+                    .foregroundColor(.gray.opacity(0.5))
+                    .padding(.top, 30)
+                
+                HStack(spacing: 8) {
+                    Text("У вас нет аккаунта?")
+                        .foregroundColor(.gray.opacity(0.5))
+
+                    Button(action: {}) {
+                        Text("Регистрация")
+                    }
+                }.padding(.top, 25)
             }
-        }
+        } .padding(.horizontal, 16)
     }
 }
 
